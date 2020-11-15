0b602165a3fde194f3f646afee983b1cf563a160
yurapob <73793102+yurapob@users.noreply.github.com>
2020-11-15 22:18:02 +0200

    Add files via upload

---
+++ AOC/Lab2/Client2.0.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static bool CheckCell(int c,ref int[] cc)
+        {
+            
+            for (int i = 0; i < 9; i++)
+            {
+                if (c == cc[i])
+                {
+                    cc[i] = 0;
+                    return true;
+                }
+            }
+            return false;
+        }
+        static int port = 8005;
+        static string address = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            int[] cc = new int[9] { 11, 12, 13, 21, 22, 23, 31, 32, 33 };
+            string[,] xo = new string[5, 5] { {"   ","|","   ","|","   "},
+                                              {"---","|","---","|","---"},
+                                              {"   ","|","   ","|","   "},
+                                              {"---","|","---","|","---"},
+                                              {"   ","|","   ","|","   "}};
+
+
+            Console.WriteLine("#EXAMPLE" + "\n" +
+                              "#To select the cell that is in the first row and in the second column, write 12" + "\n" +
+                              "#[ROW][COLUMN]" + "\n");
+
+            try
+            {
+                while (true)
+                {
+                    IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(ipPoint);
+
+                    string message;
+                    int cellX;
+                    while (true)
+                    {
+                        Console.Write("Select a cell: ");
+                        message = Console.ReadLine();
+                        if (!int.TryParse(message, out cellX)) Console.WriteLine("Error, please try again!");
+                        else
+                        {
+                            cellX = Convert.ToInt32(message);
+                            if (!CheckCell(cellX,ref cc)) Console.WriteLine("Error, please try again!");
+                            else break;
+                        }
+                    }
+
+                    xo[2 * (cellX / 10 - 1), 2 * (cellX % 10 - 1)] = " x ";
+                    void XO()
+                    {
+                        for (int i = 0; i < 5; i++)
+                        {
+                            for (int j = 0; j < 5; j++)
+                            {
+                                Console.Write(xo[i, j]);
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                    XO();
+                    
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    socket.Send(data);
+                   
+                    data = new byte[256];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (socket.Available > 0);
+
+
+                    int cellO;
+                    if (!int.TryParse(builder.ToString(), out cellO))
+                    {
+                        Console.WriteLine(builder.ToString());
+                        break;
+                    }
+                    cellO = Convert.ToInt32(builder.ToString());
+                    Console.WriteLine("Server response: " + cellO + "\n");
+                    CheckCell(cellO,ref cc);
+                    xo[2 * (cellO / 10 - 1), 2 * (cellO % 10 - 1)] = " o ";
+                    XO();
+
+
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                } 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ AOC/Lab2/Server2.0.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static bool CheckWin(int[,] xo,int wxo)
+        {
+            for (int i = 0; i < 3; i++) if (xo[i, 0] == wxo && xo[i, 1] == wxo && xo[i, 2] == wxo) return true;
+            for (int i = 0; i < 3; i++) if (xo[0, i] == wxo && xo[1, i] == wxo && xo[2, i] == wxo) return true;
+            if ((xo[0, 0] == wxo && xo[1, 1] == wxo && xo[2, 2] == wxo) || (xo[0, 2] == wxo && xo[1, 1] == wxo && xo[2, 0] == wxo)) return true;
+            return false;
+        }
+
+        static int port = 8005;
+        static void Main(string[] args)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            int draw = 1;
+            int[,] xo = new int[3, 3] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            Random rnd = new Random();
+
+            try
+            {
+                listenSocket.Bind(ipPoint);
+                listenSocket.Listen(9);
+
+                Console.WriteLine("The server is running. Waiting for connections ...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    void Terminal(string outStr)
+                    {
+                        string message = outStr;
+                        data = Encoding.Unicode.GetBytes(message);
+                        handler.Send(data);
+
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                    }
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+
+                    int cellX = Convert.ToInt32(builder.ToString());
+                    int i, j;
+                    xo[cellX / 10 - 1, cellX % 10 - 1] = 1;
+
+
+                    if (CheckWin(xo, 1))
+                    {
+                        Terminal("###YOU WIN###");
+                        break;
+                    }
+                    if (draw == 5)
+                    {
+                        Terminal("###DRAW###");
+                        break;
+                    }
+                    while (true)
+                    {
+                        i = rnd.Next(0, 3);
+                        j = rnd.Next(0, 3);
+                        if (xo[i, j] == 0)
+                        {
+                            xo[i, j] = 2;
+                            break;
+                        }
+                    }
+                    if (CheckWin(xo, 2))
+                    {
+                        Terminal("###YOU WIN###");
+                        break;
+                    }
+                    draw++;
+                    Terminal(Convert.ToString(i+1)+ Convert.ToString(j+1));   
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
